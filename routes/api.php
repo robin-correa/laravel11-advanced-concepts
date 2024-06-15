d50cf94ae6deb240672097b3cbb94fa4b326bc0b
robin.correa <robin.correa21@gmail.com>
2024-06-16 01:51:30 +0800

    Implement mailing with event and listener.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\RegisterEventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/register', [RegisterController::class, 'register']);
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/registerEvent', [RegisterEventController::class, 'register']);
