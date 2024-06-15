6a771999df8a8ac2fb3e81555fb394ba2e0594f2
robin.correa <robin.correa21@gmail.com>
2024-06-15 22:59:31 +0800

    Implement mailing with job queue.

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Jobs\SendWelcomeEmail;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle user registration.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        // Validate the request
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Create the user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Dispatch the job to send the welcome email
+        SendWelcomeEmail::dispatch($user);
+
+        // Return a response
+        return response()->json(['message' => 'User registered successfully'], Response::HTTP_CREATED);
+    }
+}
